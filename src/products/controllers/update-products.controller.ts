fc62cdb0dd92eb7851bc608c43e9b34e535a7b87
Zeus Amorim <amorimzeus@gmail.com>
2024-02-12 13:05:32 -0300

    adds the update product funcionality

--- src/products/controllers/update-products.controller.ts
+++ src/products/controllers/update-products.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Param, Put } from "@nestjs/common";
 
 import { UpdateProductsService } from "../services/update-products.service";
+import { ReadProductsService } from "../services/read-products.service";
 import { ProductDto } from "../dto/product.dto";
 
 
@@ -9,14 +10,18 @@ import { ProductDto } from "../dto/product.dto";
 export class UpdateProductsController {
 
     constructor(
-        private readonly updateProductsService: UpdateProductsService
+        private readonly updateProductsService: UpdateProductsService,
+        private readonly readProductsService: ReadProductsService
     ) { }
+
+ 
 
 
     @Put('update/:id')
     async updateProductWithId(@Param('id') id: number, @Body() updateProductData: ProductDto): Promise<any> {
         try {
             await this.updateProductsService.updateProductWithId(Number(id), updateProductData)
+            return await this.readProductsService.getSingleProductById(Number(id))
         } catch (e) {
             return e
         }
